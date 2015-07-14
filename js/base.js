87cf5d0d723eff773026100ede63a74b4465fad1
yzsunlei <846990846@qq.com>
2015-07-14 11:06:50 +0800

    modified drag module

--- js/base.js
+++ js/base.js
@@ -263,17 +263,18 @@ Base.prototype.unlock = function(){
     }
     return this;
 }
-//拖拽事件
-Base.prototype.drag = function(){
-    for(var i=0;i<this.elements.length;i++){
-		addEvent(this.elements[i], 'mousedown', function (e) {
-			if (trim(this.innerHTML).length == 0) e.preventDefault();
-            var e = getEvent(e);
-            var _this = this;
-            var diffX = e.clientX - _this.offsetLeft;
-            var diffY = e.clientY - _this.offsetTop;
 
-            if(e.target.tagName == 'h2'){
+/* 
+//拖拽事件
+Base.prototype.drag = function () {
+	for (var i = 0; i < this.elements.length; i ++) {
+		addEvent(this.elements[i],'mousedown',function(e){
+            if(trim(this.innerHTML).length == 0) e.preventDefault();
+            var _this = this;
+			var diffX = e.clientX - _this.offsetLeft;
+			var diffY = e.clientY - _this.offsetTop;
+            
+            if(e.target.tagName == 'H2'){
                 addEvent(document,'mousemove',move);
                 addEvent(document,'mouseup',up);
             }else{
@@ -282,41 +283,47 @@ Base.prototype.drag = function(){
             }
             
             function move(e){
-                var e = getEvent(e);
-                var left = e.clientX - diffX;
-                var top = e.clientY - diffY;
+				var left = e.clientX - diffX;
+				var top = e.clientY - diffY;
+				
+				if (left < 0) {
+					left = 0;
+				} else if (left > getInner().width - _this.offsetWidth) {
+					left = getInner().width - _this.offsetWidth;
+				}
+				
+				if (top < 0) {
+					top = 0;
+				} else if (top > getInner().height - _this.offsetHeight) {
+					top = getInner().height - _this.offsetHeight;
+				}
+				
+				_this.style.left = left + 'px';
+				_this.style.top = top + 'px';
                 
-                //设置不允许超过视口边缘
-                if(left < 0 ){
-                    left = 0;
-                }else if(left > getInner().width - _this.offsetWidth){
-                    left = getInner().width - _this.offsetWidth;
-                }
-                if(top < 0 ){
-                    top = 0;
-                }else if(top > getInner().height - _this.offsetHeight){
-                    top = getInner().height - _this.offsetHeight;
-                }
-                
-                _this.style.left = left + 'px';
-                _this.style.top = top + 'px';
-                
-                if(typeof _this.setCapture != 'undefined'){
-                    _this.setCapture;
-                }
+                if (typeof _this.setCapture != 'undefined') {
+                    _this.setCapture();
+                } 
             }
+            
             function up(){
-                this.onmousemove = null;
-                this.onmouseup = null;
-                if(typeof _this.releaseCapture != 'undefined'){
-                    _this.releaseCapture;
-                }
+				removeEvent(document,'mousemove',move);
+                removeEvent(document,'mouseup',up);
+				this.onmouseup = null;
+				if (typeof _this.releaseCapture != 'undefined') {
+					_this.releaseCapture();
+				}
             }
-        });
-    }
-    return this;
-}
+		});
+	}
+	return this;
+} 
 
+ */
+ //接受插件入口
+Base.prototype.extend = function(name,fn){
+    Base.prototype[name] = fn;
+}
 
 
 
